8c4adca2882475705fe910e4d165f587ca4eea51
Chin-An Kuo <133106823+Oscar7474@users.noreply.github.com>
2023-10-25 23:03:41 +0800

    Update Largest Rectangle in histogram.cpp

--- hard/Largest Rectangle in histogram.cpp
+++ hard/Largest Rectangle in histogram.cpp
@@ -24,6 +24,29 @@ public:
         return ans; 
     }
 };
+/* bad solution
+int largestRectangleArea(int* heights, int heightsSize) {
+    int* stack = (int*)malloc((heightsSize + 1) * sizeof(int));  
+    int top = -1;  
+    int maxArea = 0;
+    
+    for (int i = 0; i <= heightsSize; i++) {
+        while (top >= 0 && (i == heightsSize || heights[i] < heights[stack[top]])) {
+            
+            int height = heights[stack[top]];
+            top--;
+            int width = top >= 0 ? i - stack[top] - 1 : i;
+            int area = height * width;
+            if (area > maxArea) {
+                maxArea = area;
+            }
+        }
+        stack[++top] = i;
+    }
+    return maxArea;
+}
+*/
+
 /* time exceed
 int largestRectangleArea(int* heights, int heightsSize) {
     int maxArea = 0;
