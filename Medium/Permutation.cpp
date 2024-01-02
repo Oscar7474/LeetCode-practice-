879975ccb0bd47bcf3788c3bd105f3ae409591a4
Chin-An Kuo <133106823+Oscar7474@users.noreply.github.com>
2024-01-02 17:47:32 +0800

    Update Permutation.cpp

--- Medium/Permutation.cpp
+++ Medium/Permutation.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++){
-            
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
 
-        }
+        do {
+            result.push_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
+
+        return result;
     }
 };
